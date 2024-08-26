9985b545e3ec13c3e947b819860151bce89b7433
SENA <mmm808383@gmail.com>
2024-08-26 06:59:01 -0500

    Getter and setter cliente.java and factura.java

--- src/generator/cliente.java
+++ src/generator/cliente.java
@@ -2,5 +2,25 @@ package generator;
 
 public class cliente {
     private String Nombre;
-    private String nif;//Numero de identificacion fiscal
+    private String nif;//Numero de identificacion fiscal.
+
+
+    //Getter and setters
+
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public String getNif() {
+        return nif;
+    }
+
+    public void setNif(String nif) {
+        this.nif = nif;
+    }
 }
--- src/generator/factura.java
+++ src/generator/factura.java
@@ -7,4 +7,57 @@ public class factura {
     private Date fecha;
     private cliente cliente;
     private itemFactura[] items;
+
+    //getter and setters
+
+
+    //Folio
+    public int getFolio() {
+        return folio;
+    }
+
+    public void setFolio(int folio) {
+        this.folio = folio;
+    }
+
+    //Descripcion
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    //Fecha
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    //Cliente
+
+    public generator.cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(generator.cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    //ItemFcatura
+
+
+    public itemFactura[] getItems() {
+        return items;
+    }
+
+    public void setItems(itemFactura[] items) {
+        this.items = items;
+    }
 }
