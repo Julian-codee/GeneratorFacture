a911d961072e6af55749fce28cb353620ba7c32c
SENA <mmm808383@gmail.com>
2024-08-26 07:01:41 -0500

    Getter and setter itemFactura and producto

--- src/generator/itemFactura.java
+++ src/generator/itemFactura.java
@@ -3,4 +3,24 @@ package generator;
 public class itemFactura {
     private producto producto;
     private int cantidad;
+
+    //Getters and setters
+
+    //Producto
+    public generator.producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(generator.producto producto) {
+        this.producto = producto;
+    }
+
+    //Cantidad
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
 }
--- src/generator/producto.java
+++ src/generator/producto.java
@@ -4,4 +4,36 @@ public class producto {
     private String codigo;
     private String nombre;
     private double precio;
+
+    //Getters and setters
+
+    //codigo
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    //Nombre
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    //Precio
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
 }
