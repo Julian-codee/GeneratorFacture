37b3467256b6cdbf37698104fd2752e7f338527e
Julian Cortes <mmm808383@gmail.com>
2024-08-28 19:09:32 -0500

    Creacion  y importaciones del package , creacion de la public static class

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import generator.producto;
+import generator.itemFactura;
+import generator.factura;
+import generator.cliente;
+import java.util.Scanner;
+
+public class Main {
+    //Creacion del nuevo cliente
+    public static void main(String[] args) {
+        cliente cliente = new cliente();
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
